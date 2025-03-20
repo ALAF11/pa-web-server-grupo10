a63d1003b1cc1e3e3d7aebdfda078e3cb377b4c6
alaf_01 <2081821@student.uma.pt>
2025-03-20 21:11:04 +0000

    feat: new implementation of LogConsumer

--- src/main/java/LogConsumer.java
+++ src/main/java/LogConsumer.java
@@ -1,39 +1,64 @@
-import java.io.FileWriter;
+import java.io.*;
+import java.rmi.server.LoaderHandler;
 import java.time.LocalDateTime;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class LogConsumer {
-    private static final BlockingQueue<LogEntry> logQueue = new LinkedBlockingQueue<>();
-    private static volatile boolean running = true;
+public class LogConsumer implements Runnable {
+    private BlockingQueue<LogEntry> logQueue;
+    private final String logFilePath;
 
-    public static void startLogThread() {
-        Thread logThread = new Thread(() -> {
-            while (running || !logQueue.isEmpty()) {
-                try {
-                    LogEntry entry = logQueue.take();
-                    writeToFile(entry);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                } catch (Exception e) {
-                    System.err.println("Log error: " + e.getMessage());
-                }
-            }
-        });
-        logThread.start();
+    public LogConsumer(BlockingQueue<LogEntry> logQueue, String logFilePath){
+        this.logQueue = logQueue;
+        this.logFilePath = logFilePath;
+
     }
-    private static synchronized void writeToFile(LogEntry entry){
-        try(FileWriter writer =new FileWriter("server.log",true)){
-            writer.write(entry.toJSON()+ "\n");
-        } catch (Exception e) {
-            System.err.println("Failed to write log: " + e.getMessage());
+
+    @Override
+    public void run() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFilePath, true))){
+           while(true){
+               LogEntry logEntry = logQueue.take(); //this call blocks until an entry is available
+
+               writer.write(logEntry.toJSON());
+               writer.newLine(); //
+               writer.flush();
+           }
+        }
+        catch (IOException | InterruptedException e){
+            e.printStackTrace();
         }
     }
-    public static void logRequest(LocalDateTime timestamp, String method,
-                                  String route, String origin, int status) {
-        logQueue.add(new LogEntry(timestamp, method, route, origin, status));
-    }
-    public static void shutdown() {
-        running = false;
-    }
+
+//    public static void startLogThread() {
+//        Thread logThread = new Thread(() -> {
+//            while (running || !logQueue.isEmpty()) {
+//                try {
+//                    LogEntry entry = logQueue.take();
+//                    writeToFile(entry);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                } catch (Exception e) {
+//                    System.err.println("Log error: " + e.getMessage());
+//                }
+//            }
+//        });
+//        logThread.start();
+//    }
+//    private static synchronized void writeToFile(LogEntry entry){
+//        try(FileWriter writer =new FileWriter("server.log",true)){
+//            writer.write(entry.toJSON()+ "\n");
+//        } catch (Exception e) {
+//            System.err.println("Failed to write log: " + e.getMessage());
+//        }
+//    }
+//    public static void logRequest(LocalDateTime timestamp, String method,
+//                                  String route, String origin, int status) {
+//        logQueue.add(new LogEntry(timestamp, method, route, origin, status));
+//    }
+//    public static void shutdown() {
+//
+//        running = false;
+//    }
+
 }
