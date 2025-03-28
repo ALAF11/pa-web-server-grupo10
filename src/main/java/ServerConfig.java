bb57ffb4402f3a4c83a6d56907449599b1a83c6d
MarcosAFFernandes <maffernandes@live.com.pt>
2025-03-28 15:08:09 +0000

    refactor: refactor ServerConfig

--- src/main/java/ServerConfig.java
+++ src/main/java/ServerConfig.java
@@ -1,9 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ServerConfig {
-    private final ReentrantLock lock = new ReentrantLock();
+
     private final Map<String, String> configMap;
 
     public ServerConfig() {
@@ -11,30 +10,15 @@ public class ServerConfig {
     }
 
     public void setConfig(String key, String value) {
-        lock.lock();
-        try {
-            configMap.put(key, value);
-        } finally {
-            lock.unlock();
-        }
+        configMap.put(key, value);
     }
 
     public String getConfig(String key) {
-        lock.lock();
-        try {
-            return configMap.get(key);
-        } finally {
-            lock.unlock();
-        }
+        return configMap.get(key);
     }
 
 
     public int getIntConfig(String key) {
-        lock.lock();
-        try {
-            return Integer.parseInt(configMap.get(key));
-        } finally {
-            lock.unlock();
-        }
+        return Integer.parseInt(configMap.get(key));
     }
 }
